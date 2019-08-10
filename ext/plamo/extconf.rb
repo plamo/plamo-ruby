f626cd5a5719901162905ce152424d568286797c
Shogo Otake <shogo.otake@gmail.com>
2019-08-11 04:55:43 +0900

    Update extconf.rb

--- ext/plamo/extconf.rb
+++ ext/plamo/extconf.rb
@@ -1,3 +1,11 @@
 require "mkmf"
 
+unless find_header("libplamo.h")
+  abort "libplamo is missing. please install libplamo"
+end
+
+unless find_library("plamo", "plamo_string_new")
+  abort "libplamo is missing. please install libplamo"
+end
+
 create_makefile("plamo/plamo")
