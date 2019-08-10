aa4b455685f909dd3c582f77b35dd73a928e1ec4
Shogo Otake <shogo.otake@gmail.com>
2019-08-11 04:39:33 +0900

    Add PlamoHttpHeader

--- ext/plamo/plamo.c
+++ ext/plamo/plamo.c
@@ -5,4 +5,5 @@ VALUE rb_mPlamo;
 void Init_plamo(void) {
   rb_mPlamo = rb_define_module("Plamo");
   Init_plamo_string_array();
+  Init_plamo_http_header();
 }
---
+++ ext/plamo/plamo_http_header.h
@@ -0,0 +1,8 @@
+#ifndef RUBY_PLAMO_HTTP_HEADER_H
+#define RUBY_PLAMO_HTTP_HEADER_H
+
+extern VALUE rb_cPlamoHttpHeader;
+
+void Init_plamo_http_header(void);
+
+#endif /* RUBY_PLAMO_HTTP_HEADER_H */
